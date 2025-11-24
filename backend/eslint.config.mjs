71bec2b8f760e73eab7b63de0783c3c0d58ba4e8
Diego Maldonado <100791257+2diego@users.noreply.github.com>
2025-11-24 15:00:15 -0300

    Rutina: Fix modulos y servicios relacionados
    Rutina: Fix modulos y servicios relacionados (dia, semana, rutina, ejercicio, y relacion con usuario)

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -29,6 +29,9 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn'
+      ,
+      // Desactivar reglas de prettier para que no aparezcan como errores en ESLint
+      'prettier/prettier': 'off',
     },
   },
 );
